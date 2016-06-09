8bf02a5589ed05be49b33bc8d39c1db33a141209
Deep Sodhi <Deep Sodhi>
2016-06-09 21:15:22 +0530

    document.write()

---
+++ Question 1/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  document.write("Web Broswer : " + navigator.appName);
+  document.write("<br />");
+  document.write("Height : " + window.screen.availHeight);
+  document.write("<br />");
+  document.write("Width : " + window.screen.availWidth);
+  document.write("<br />");
+  document.write("Last Modified : " + document.lastModified);
+}
